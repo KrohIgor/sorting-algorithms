360d3d7985e576347d074b016fc8dcefb675902e
ihor <krohmal.igor777@gmail.com>
2020-03-08 18:11:41 +0200

    added method sort()

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,5 +1,26 @@
+import java.util.Arrays;
+
 public class BubbleSort {
-    public static int[] sort(int[] array){
-        return new int[0];
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sort(new int[]{1, 8, 4, 6, 2, 7, 3, 15, 5})));
+    }
+
+    public static int[] sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean permutationsWere = false;
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    permutationsWere = true;
+                }
+            }
+            if (!permutationsWere) {
+                break;
+            }
+        }
+        return array;
     }
 }
